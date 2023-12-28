de2f452745b18450de9490475ac3b2f9ce94d239
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2023-12-28 14:51:08 +0300

    fix: fix triangle task

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -4,7 +4,7 @@ public class Triangle {
     public void printTriangle(int cathetusLength) {
         for (int i = 1; i <= cathetusLength; i++) {
             for (int j = 1; j <= cathetusLength; j++) {
-                if (j == 1 || i == j || i == cathetusLength) {
+                if (j == 1 || i >= j || i == cathetusLength) {
                     System.out.print(8);
                 } else {
                     System.out.print(" ");
@@ -12,5 +12,9 @@ public class Triangle {
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        new Triangle().printTriangle(6);
     }
 }
