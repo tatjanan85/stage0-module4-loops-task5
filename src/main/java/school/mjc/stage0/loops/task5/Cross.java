62e20cd0ffd3f605753e2d82efc59f0c23927eac
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2023-12-28 14:33:06 +0300

    fix: fix cross task

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -4,7 +4,7 @@ public class Cross {
     public void printCross(int sideLength) {
         for (int i = 1; i <= sideLength; i++) {
             for (int j = 1; j <= sideLength; j++) {
-                if (i == j || i + j == sideLength + 1) {
+                if (i == (sideLength + 1) / 2 || j == (sideLength + 1) / 2) {
                     System.out.print(8);
                 } else {
                     System.out.print(" ");
@@ -12,5 +12,9 @@ public class Cross {
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        new Cross().printCross(3);
     }
 }
